901a18c410600ad0f8de4032d6b1f8d5a746d8e9
Bryan Mahoney <bryan@goDynamo.com>
2012-03-02 18:45:22 -0500

    Ensures default page parts are assigned to newly cloned pages. Clone page structure using delayed job.

--- app/models/season.rb
+++ app/models/season.rb
@@ -22,12 +22,17 @@ class Season < ActiveRecord::Base
     clone_pages(Season.first.pages.where(:parent_id => nil), nil, 0)
   end
   
+  handle_asynchronously :copy_page_structure_from_default_season
+  
   def clone_pages(pages, parent=nil, depth)
     depth += 1
     return if depth == 3
     pages.each do |page|
       cloned_page = page.clone
       cloned_page.parent = parent
+      Page.default_parts.each_with_index do |page_part, index|
+        cloned_page.parts << PagePart.new(:title => page_part, :position => index)
+      end
       self.pages << cloned_page
       clone_pages(page.children, cloned_page, depth)
     end
