b3bd13d2584b78afd4b84da65b5e64301cf9eddc
Bryan Mahoney <bryan@goDynamo.com>
2012-03-02 18:55:42 -0500

    Scopes the pages#home method to the current season

---
+++ lib/extensions/seasons_pages_controller_extension.rb
@@ -0,0 +1,13 @@
+module SeasonsPagesControllerExtension
+  extend ActiveSupport::Concern
+
+  module InstanceMethods
+  
+    def home
+      error_404 unless (@page = Season.current.pages.where(:link_url => '/').first).present?
+    end
+  end
+
+end
+
+PagesController.send(:include, SeasonsPagesControllerExtension)
